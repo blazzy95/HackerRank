6a68fc0e70a4af7dc3736824bb5f58b023a6935a
Jitendra Selvam <jitendra3195@gmail.com>
2017-12-26 17:40:21 -0500

    HackerRankSolutions

---
+++ Greedy/MarcCupCakes.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApp9
+{
+    class MarcCupCakes
+    {
+        static void Main(String[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+            string[] calories_temp = Console.ReadLine().Split(' ');
+            int[] calories = Array.ConvertAll(calories_temp, Int32.Parse);
+            // your code goes here
+
+            Array.Sort(calories);
+            double miles = 0;
+            int eaten = 0;
+
+            for (int i = n - 1; i >= 0; i--)
+            {
+                miles = (miles + (Math.Pow(2, eaten) * calories[i]));
+                eaten++;
+            }
+            Console.WriteLine(miles);
+            Console.ReadKey();
+        }
+    }
+}
