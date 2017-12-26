c57c735d3080b687c627159d331a3524c95a5f6a
Jitendra Selvam <jitendra3195@gmail.com>
2017-12-26 17:56:45 -0500

    HackerRank Solution

---
+++ Greedy/MinAbsDiffArr.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+/// <summary>
+/// Minimum Absolute Difference in an Array from Greedy Algorithm Hacker Rank solution.
+/// Author: Jitendra Selvam
+/// </summary>
+
+namespace ConsoleApp1
+{
+    class MinAbsDiffArr
+    {
+        static int minimumAbsoluteDifference(int n, int[] arr)
+        {
+            // Complete this function
+            Array.Sort(arr);
+            int min = Int32.MaxValue;
+
+            for(int i = 1; i<arr.Length;i++)
+            {
+                min = Math.Min(Math.Abs(arr[i]-arr[i-1]),min);
+            }
+            return min;
+        }
+
+        static void Main(String[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+            string[] arr_temp = Console.ReadLine().Split(' ');
+            int[] arr = Array.ConvertAll(arr_temp, Int32.Parse);
+            int result = minimumAbsoluteDifference(n, arr);
+            Console.WriteLine(result);
+            Console.Read();
+        }
+    }
+}
